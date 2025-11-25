530ae48a5fdec49cf4d21194260de558aa63e3d7
blue-crab-uu <zhudh121@gmail.com>
2025-11-25 10:57:29 +0800

    app.js测试可运行

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 导入模型
-import Todo from '../models/Todomodels.js';
+import Todo from './src/models/Todomodels.js';
+import sequelize from './src/utils/dbhelp.js';
 
 // 查询全部数据
 async function getTodos() {
@@ -8,7 +9,7 @@ async function getTodos() {
 }
 // 过滤查询
 async function getTodosByFilter(filter) {
-  const todos = await Todo.findAll({ where: filter });
+  const todos = await Todo.findOne({ where: filter });
   return todos;
 }
 
@@ -17,22 +18,61 @@ async function createTodo(todoData) {
   const todo = await Todo.create(todoData);
   return todo;
 }
-// 更新数据
+// 根据id更新数据
 async function updateTodo(id, todoData) {
   const todo = await Todo.findByPk(id);
   if (todo) {
     await todo.update(todoData);
   }
+  else {
+    console.log(`Todo not found`);
+  }
   return todo;
 }
-// 删除数据
+// 根据id删除数据
 async function deleteTodo(id) {
   const todo = await Todo.findByPk(id);
   if (todo) {
     await todo.destroy();
   }
+  else {
+    console.log(`Todo not found`);
+  }
   return todo;
 }
 
+// 导出所有 CRUD 函数
+export {
+  getTodos,
+  getTodosByFilter,
+  createTodo,
+  updateTodo,
+  deleteTodo
+};
 
 
+
+// // 查询全部数据
+// getTodos().then(todos => console.log(todos));
+
+// // 过滤查询
+// getTodosByFilter({ id: '2' }).then(todos => console.log(todos));
+
+// // 新增数据
+// createTodo({
+//   title: 'Sequelize',
+//   content: 'use Sequelize',
+//   tag: 'learn'
+// })
+//   .then(todo => console.log('Created Todo:', todo))
+//   .catch(err => console.error('创建失败:', err));
+
+// // 更新数据
+// updateTodo('1', { title: 'Updated Todo' })
+//   .then(todo => console.log('Updated Todo:', todo))
+//   .catch(err => console.error('更新失败:', err));
+
+// // 删除数据
+// deleteTodo('6')
+//   .then(todo => console.log('Deleted Todo:', todo))
+//   .catch(err => console.error('删除失败:', err));
