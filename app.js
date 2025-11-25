1191033c1da0257150c0e2d185e3695555e7c25d
blue-crab-uu <zhudh121@gmail.com>
2025-11-25 14:33:20 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -76,3 +76,72 @@ export {
 // deleteTodo('6')
 //   .then(todo => console.log('Deleted Todo:', todo))
 //   .catch(err => console.error('删除失败:', err));
+
+
+// raw query
+// // 类型一
+// import { QueryTypes } from 'sequelize';
+// const todos = await sequelize.query('SELECT * FROM `todo`', {
+//   type: QueryTypes.SELECT,
+// });
+// // We didn't need to destructure the result here - the results were returned directly
+// console.log(todos);
+
+// // 类型二 映射到模型
+// import { QueryTypes } from 'sequelize';
+// const projects = await sequelize.query('SELECT * FROM `todo`', {
+//   model: Todo,
+//   mapToModel: true, // pass true here if you have any mapped fields
+// });
+// // We didn't need to destructure the result here - the results were returned directly
+// console.log(projects);
+
+//  未命名参数（? 顺序替换）
+// import { QueryTypes } from 'sequelize';
+
+// const projects = await sequelize.query('SELECT * FROM `todo` WHERE `tag` = ?', {
+//   replacements: ['life'],
+//   type: QueryTypes.SELECT,
+// });
+
+// console.log('结果：', projects);
+
+// // 命名参数（:name 替换）
+// import { QueryTypes } from 'sequelize';
+
+// const projects = await sequelize.query('SELECT * FROM `todo` WHERE `tag` = :tag', {
+//   replacements: { tag: 'life' },
+//   type: QueryTypes.SELECT,
+// });
+
+// console.log('结果：', projects);
+
+// //数组in查询
+// import { QueryTypes } from 'sequelize';
+
+// const projects = await sequelize.query('SELECT * FROM `todo` WHERE `tag` IN (:tags)', {
+//   replacements: { tags: ['life', 'travel'] },
+//   type: QueryTypes.SELECT,
+// });
+
+// console.log('结果：', projects);
+
+// // 通配符查询
+// import { QueryTypes } from 'sequelize';
+
+// const projects = await sequelize.query('SELECT * FROM `todo` WHERE `content` LIKE :content', {
+//   replacements: { content: '%to%' },
+//   type: QueryTypes.SELECT,
+// });
+
+// console.log('结果：', projects);
+
+// 数组绑定（:name 替换）
+import { QueryTypes } from 'sequelize';
+
+const projects = await sequelize.query('SELECT * FROM `todo` WHERE `id` = $1', {
+  bind: ['1'],
+  type: QueryTypes.SELECT,
+});
+
+console.log('结果：', projects);
